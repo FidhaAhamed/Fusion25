5b9cfa950d137fbf0940c98d1f87c71a8bf2f900
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-01-20 23:25:35 +0530

    minor updates

--- src/components/RegBtn.jsx
+++ src/components/RegBtn.jsx
@@ -1,4 +1,21 @@
- import '../index.css'
+import '../index.css'
+
+
+ const RegBtn = () => {
+   
+   return (
+     <button className="reg-btn-gradient px-4 sm:px-8 md:px-16 py-2 md:py-4 rounded-[50px] text-white text-sm sm:text-lg md:text-xl font-semibold flex items-center gap-x-2 whitespace-nowrap uppercase"
+     >
+         <h1>REGISTER NOW</h1>
+         <img src='/Images/reg-btn-icon.png'/>
+     </button>
+   )
+ }
+ export default RegBtn
+
+
+
+{/** import '../index.css'
 
 
  const RegBtn = () => {
@@ -13,7 +30,7 @@
      </button>
    )
  }
- export default RegBtn
+ export default RegBtn}**/}
 
 {/**import '../index.css'
 
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         FUSION 2025 is an exciting three-day event hosted by IEEE SPS SBC MACE in 
         collaboration with the IEEE SPS Kerala Chapter, taking place on February 21, 
         22, and 23, 2025. Designed to inspire and engage students, FUSION is a 
-        vibrant mix of technical sessions, hands-on workshops, a thrilling hackathon, 
+        vibrant mix of technical sessions, hands-on workshops, a thrilling ideathon, 
         entertaining games, and cultural programs. It's the perfect platform to learn, 
         innovate, and connect with like-minded peers.
       </p>
