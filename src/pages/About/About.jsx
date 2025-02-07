5fc8cf4b81c222d218dfe4bab2ea2f1d406127cd
Ajay Susanth <ajaysusanth10@gmail.com>
2025-02-08 01:17:50 +0530

    reg btn changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,13 +11,13 @@ const About = () => {
 
       <Heading heading="about fusion"/>
 
-      <p className="about my-8 text-base md:text-lg leading-relaxed font-ClashDisplay text-center max-w-xs md:max-w-3xl mx-auto mb-8 px-6 md:px-0">
-      FUSION'25 is a functional 3-day event held by IEEE SPS SBC MACE in collaboration with IEEE SPS Kerala Chapter. 
+      <p className="about tracking-wide my-8 text-base md:text-lg leading-relaxed font-ClashDisplay text-center max-w-xs md:max-w-3xl mx-auto mb-8 px-6 md:px-0">
+      FUSION'25 is a dynamic 3-day event held by IEEE SPS SBC MACE in collaboration with IEEE SPS Kerala Chapter. 
       The event commences on 21st and continues through 22nd to culminate on the 23rd February 2025. The workshops and talk sessions 
       incorporated into the event are sure to spark passion for Signal Processing in minds of the attendees. The practical experiences 
       provided will also make one confident in their own talents. The event also includes an ideathon to challenge and urge the participants 
       to find innovative solutions to delicate yet perilous problems. To not get drowned out by all the technical traits, FUSION also conducts 
-      interative games for the thrill of it and cultural night to let loose. The most pivotal take away from the event is the invaluable 
+      interactive games for the thrill of it and cultural night to let loose. The most pivotal take away from the event is the invaluable 
       connections made during the undertaking, which will forever be an asset in your life. So come and be a part of this extraordinary 
       event to experience and learn something new and enthralling.
       </p>
